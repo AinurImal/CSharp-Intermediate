6d4935baf78607a2d58bc4542cd102856ff5a41a
AinurImal <imalainur@gmail.com>
2025-04-09 11:58:17 +0800

    4 - Inheritance

---
+++ 4 - Inheritance/23. Constructor and Inheritance/Constructor and Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Constructors_and_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the base (Vehicle) class of the car class is allways initialized first
+            var car = new Car("XYZ 32-476");
+
+            Console.ReadKey();
+        }
+    }
+}
